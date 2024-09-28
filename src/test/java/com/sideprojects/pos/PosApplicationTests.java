f8d42cadc1ab1a333db9c59f5a71fec48465df34
danymorar20 <danymorar20@gmail.com>
2024-09-28 17:52:33 -0600

    initial commit

---
+++ src/test/java/com/sideprojects/pos/PosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideprojects.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
